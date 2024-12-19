8217001a477e3b149b13074028f140195306eaa0
TTibbs <terryjw.97@gmail.com>
2024-12-19 16:23:01 +0000

    Feat: Setup nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="home" />
+      <Stack.Screen name="profile" />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="w-1/2 mx-auto">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/profile" className="bg-zinc-900 text-zinc-100 py-2 px-3">
+        Profile
+      </Link>
     </View>
   );
 }
